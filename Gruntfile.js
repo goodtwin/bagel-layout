2a82b59c3223e3cc7376f0891bc8c02908f9c7f8
Greg Thompson <greg@good-twin.com>
2014-08-18 17:02:20 -0500

    v0.1.0

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+/*global module, require*/
+module.exports = function (grunt) {
+  'use strict';
+
+  var globalConfig = {
+    src: 'init.scss',
+    dist: 'init.css'
+  };
+
+  grunt.initConfig({
+    globalConfig: globalConfig,
+    pkg: grunt.file.readJSON('./package.json'),
+    shared_config: {
+      style: {
+        options: {
+          name: 'defaultConfig',
+          cssFormat: 'dash',
+          useSassMaps: true
+        },
+        src: ['config.yml', 'node_modules/bagel-functions/config.yml'],
+        dest: [
+          'config.scss'
+        ]
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          loadPath: ['./', 'node_modules/']
+        },
+        files : {
+          '<%= globalConfig.dist %>': '<%= globalConfig.src %>'
+        }
+      }
+    },
+    myth: {
+      options: {
+        sourcemap: true
+      },
+      dist: {
+        files: {
+          '<%= globalConfig.dist %>': '<%= globalConfig.dist %>'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: ['**/*.scss'],
+        tasks: ['distcss']
+      }
+    }
+  });
+
+require('load-grunt-tasks')(grunt);
+
+grunt.registerTask('default', ['build']);
+grunt.registerTask('distcss', ['sass:dist', 'myth:dist']);
+grunt.registerTask('build', ['shared_config', 'distcss']);
+
+};
